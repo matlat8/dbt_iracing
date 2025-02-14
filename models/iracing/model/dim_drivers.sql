e18c88699f688788d5d0423602ae75f23fbe32aa
Matthew Latham <50916823+matlat8@users.noreply.github.com>
2025-02-14 15:55:53 -0500

    Update dim_drivers.sql

--- models/iracing/model/dim_drivers.sql
+++ models/iracing/model/dim_drivers.sql
@@ -2,11 +2,14 @@
 
 WITH data_cte AS (
     SELECT
-        cust_id,
-        display_name,
-        club_name,
-        country_code
-    FROM iracing.session_results
+        sr.cust_id,
+        sr.display_name,
+        sr.club_name,
+        sr.country_code,
+        ca.country_image_url
+    FROM iracing.session_results sr
+    LEFT JOIN iracing.country_attribution ca
+        on sr.country_code = ca.country_code
     WHERE cust_id <> 0
     QUALIFY ROW_NUMBER() OVER (
         PARTITION BY cust_id
